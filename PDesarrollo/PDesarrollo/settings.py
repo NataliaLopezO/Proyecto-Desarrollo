eaeade1e9cd5f807fb42e3caf2d34feda78b6781
NataliaLopezO <106278490+NataliaLopezO@users.noreply.github.com>
2023-02-01 13:07:06 -0500

    Update settings.py

--- PDesarrollo/PDesarrollo/settings.py
+++ PDesarrollo/PDesarrollo/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/usmelectrica/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/usmelectrica/static/'), 
+    os.path.join(BASE_DIR, 'usmelectrica/static'), 
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
